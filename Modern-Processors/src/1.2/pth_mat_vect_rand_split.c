850d51cf958d1078c5b22b6af51788006966a20b
sdi2000064@linux28 <sdi2000064@di.uoa.gr>
2022-12-03 16:59:22 +0200

    Commit

--- Modern-Processors/src/1.2/pth_mat_vect_rand_split.c
+++ Modern-Processors/src/1.2/pth_mat_vect_rand_split.c
@@ -70,7 +70,7 @@ int main(int argc, char *argv[])
    while (thread_count <= 0 || (m % thread_count) != 0)
    {
       printf("m %% thread_count != 0. Give a different number of threads: ");
-      if (scanf("%d", &thread_count) != 0)
+      if (scanf("%d", &thread_count) == 0)
       {
          perror("Invalid imput characters\n");
          return EXIT_FAILURE;
