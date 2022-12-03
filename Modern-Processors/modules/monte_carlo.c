a96b1bcf6d02da62cfa32a6f4ce33492659b2aa3
kanellam <sdi2000064@di.uoa.gr>
2022-12-03 14:51:56 +0200

    Commit

--- Modern-Processors/modules/monte_carlo.c
+++ Modern-Processors/modules/monte_carlo.c
@@ -1,37 +1,18 @@
 #include <stdlib.h>
-#include <stdbool.h>
 #include <stdio.h>
-#define _MY_RAND_H_
-#define MR_MULTIPLIER 279470273
-#define MR_INCREMENT 0
-#define MR_MODULUS 4294967291U
-#define MR_DIVISOR ((double)4294967291U)
-unsigned my_rand(unsigned* seed_p) {
-   long long z = *seed_p;
-   z *= MR_MULTIPLIER; 
-// z += MR_INCREMENT;
-   z %= MR_MODULUS;
-   *seed_p = z;
-   return *seed_p;
-}
-
-long double my_drand(unsigned* seed_p) {
-   unsigned x = my_rand(seed_p);
-   long double y = -1 + 2 * x / MR_DIVISOR;
-   return y;
-}
+#include "../include/my_rand.h"
 
 long long int monte_carlo(long long int throws)
 {
-    unsigned seed=1,tmp;
-    long long int arrows1=0;
-    long double x,y;
-    tmp=my_rand(&seed);
+    unsigned seed = 1, tmp;
+    long long int arrows1 = 0;
+    long double x, y;
+    tmp = my_rand(&seed);
     for (int i = 0; i < throws; i++)
     {
-        tmp=my_rand(&tmp);
-        x=my_drand(&tmp);
-        y=my_drand(&tmp);
+        tmp = my_rand(&tmp);
+        x = my_drand(&tmp);
+        y = my_drand(&tmp);
         long double distance = x * x + y * y;
         if (distance <= 1)
             arrows1++;
--- Modern-Processors/src/main.c
+++ Modern-Processors/src/main.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
-#include <assert.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <sys/time.h>
-#include "../include/monte_carlo.h"
 #include <pthread.h>
-#include <unistd.h>
-#include <sys/syscall.h>
+
+#include "../include/monte_carlo.h"
+#include "../include/my_rand.h"
+#include "../include/timer.h"
+
+pthread_mutex_t mutex;
 
 int thread_count;
-pthread_barrier_t barrier1;
-#define GET_TIME(now)                           \
-    {                                           \
-        struct timeval t;                       \
-        gettimeofday(&t, NULL);                 \
-        now = t.tv_sec + t.tv_usec / 1000000.0; \
-    }
-
 long long int throws;
-
-pthread_mutex_t mutex1;
 long long int arrows = 0;
 void *Hello(void *rank)
 {
@@ -39,10 +29,9 @@ void *Hello(void *rank)
         if (distance <= 1)
             my_arrows++;
     }
-    // printf("Ok from %ld\n", my_rank);
-    pthread_mutex_lock(&mutex1);
+    pthread_mutex_lock(&mutex);
     arrows += my_arrows;
-    pthread_mutex_unlock(&mutex1);
+    pthread_mutex_unlock(&mutex);
     return NULL;
 }
 
@@ -50,29 +39,24 @@ int main(int argc, char **argv)
 {
     double start, end;
     pthread_t *thread_id;
-    srand(1);
     if (argc < 3)
     {
         printf("Not enough arguments\n");
         return 1;
     }
-
     throws = strtoll(argv[1], NULL, 10);
+
     GET_TIME(start);
     long long int arrows1 = monte_carlo(throws);
-    printf("%lld \n", arrows1);
     long double pi = 4 * arrows1 / ((long double)throws);
     GET_TIME(end);
     double duration = end - start;
     printf("%Lf \n", pi);
     printf("Time: %f\n", duration);
 
-    pthread_mutex_init(&mutex1, NULL);
+    pthread_mutex_init(&mutex, NULL);
     thread_count = strtol(argv[2], NULL, 10);
     thread_id = malloc(thread_count * sizeof(pthread_t));
-    // pthread_barrier_init(&barrier1, NULL, thread_count);
-    printf("Hello World from master thread\n");
-    // arrows=0;
 
     GET_TIME(start);
     for (long thread = 0; thread < thread_count; thread++)
@@ -80,17 +64,14 @@ int main(int argc, char **argv)
 
     for (long thread = 0; thread < thread_count; thread++)
         pthread_join(thread_id[thread], NULL);
-    // GET_TIME(start);
-    // long long int arrows = monte_carlo(throws);
+        
     pi = 4 * arrows / ((long double)throws);
-    pthread_mutex_destroy(&mutex1);
-    // GET_TIME(end);
     GET_TIME(end);
     duration = end - start;
     printf("%Lf \n", pi);
-
     printf("Time: %f\n", duration);
 
+    pthread_mutex_destroy(&mutex);
     free(thread_id);
     return EXIT_SUCCESS;
 }
