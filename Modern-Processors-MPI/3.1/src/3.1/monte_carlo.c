16bc7c388684dfbf00ac6b26c4843d33517c9c4c
sdi2000064@linux28 <sdi2000064@di.uoa.gr>
2023-01-12 15:32:19 +0200

    Commit
    Co-authored-by: Michalis Chatzispyrou <Irrelev4nt13@users.noreply.github.com>

---
+++ Modern-Processors-MPI/3.1/src/3.1/monte_carlo.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include "../../include/my_rand.h"
+#include "../../include/timer.h"
+
+long long int monte_carlo();
+
+int main(int argc, char *argv[])
+{
+    int comm_sz; /* Number of processes    */
+    int my_rank; /* My process rank        */
+    long long int arrows = 0, local_arrows = 0, throws, local_throws = 0, seg;
+    long double pi;
+    MPI_Comm comm;
+
+    double start, end;
+    MPI_Init(NULL, NULL);
+    comm = MPI_COMM_WORLD;
+    /* Get the number of processes */
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+
+    /* Get my rank among all the processes */
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    if (my_rank == 0)
+    {
+        if (argc != 2)
+        {
+            perror("Wrong number of arguments");
+            return 1;
+        }
+        throws = strtol(argv[1], NULL, 10);
+    }
+    GET_TIME(start);
+    MPI_Bcast(&throws, 1, MPI_LONG_LONG_INT, 0, comm);
+    local_throws = throws;
+    seg = local_throws / comm_sz;
+
+    local_arrows = monte_carlo((my_rank + 1) * seg, my_rank * seg, my_rank + 1);
+    MPI_Reduce(&local_arrows, &arrows, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (my_rank == 0)
+    {
+        pi = 4 * arrows / (long double)throws;
+        GET_TIME(end);
+        double duration = end - start;
+        printf("%Lf %f\n", pi, duration);
+    }
+    MPI_Finalize();
+
+    return 0;
+}
+long long int monte_carlo(long long int throws, long long int start, int id)
+{
+    unsigned seed = id, tmp;
+    long long int my_arrows = 0, i;
+    long double x, y;
+    for (i = start; i < throws; i++)
+    {
+        x = -1 + 2 * my_drand(&seed);
+        y = -1 + 2 * my_drand(&seed);
+        long double distance = x * x + y * y;
+        if (distance <= 1)
+            my_arrows++;
+    }
+    return my_arrows;
+}
