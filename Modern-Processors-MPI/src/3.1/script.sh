0818f8999053a3039330a7bc4cfcab18248853c4
sdi2000064@linux28 <sdi2000064@di.uoa.gr>
2023-01-12 17:36:18 +0200

    Co-authored-by: Michalis Chatzispyrou <Irrelev4nt13@users.noreply.github.com>

--- Modern-Processors-MPI/src/3.1/script.sh
+++ Modern-Processors-MPI/src/3.1/script.sh
@@ -12,7 +12,7 @@ NEW_FILE="averages.txt"
 rm -f ${NEW_FILE}
 touch ${NEW_FILE}
 echo "Running..."
-for j in {1..4} 
+for j in {1..5} 
 do  
     throws=$throws_step
     for i in {1..10}
@@ -29,8 +29,6 @@ do
         declare -a values=(
         [0]=0
         [1]=0
-        # [2]=0
-        # [3]=0
         )
         counter=0
         while IFS= read -r line
@@ -39,26 +37,17 @@ do
             do    
                 if [ $f == "#" ]
                 then 
-                    # echo "#" >> ${NEW_FILE}
                     break
                 elif [ $f == $throws ]
                 then
-                    # echo $line >> ${NEW_FILE} 
                     break
                 else
                     read -ra split <<< "$line"
-                    if [ $(( counter % 2 )) == 0 ]
-                    then
-                        values[0]=$(echo ${values[0]} + ${split[0]} | bc -l | sed 's/^\./0./' )
-                        values[1]=$(echo ${values[1]} + ${split[1]} | bc -l | sed 's/^\./0./' )
-                    # else
-                    #     values[2]=$(echo ${values[2]} + ${split[0]} | bc -l | sed 's/^\./0./' )
-                    #     values[3]=$(echo ${values[3]} + ${split[1]} | bc -l | sed 's/^\./0./' )
-                    fi
+                    values[0]=$(echo ${values[0]} + ${split[0]} | bc -l | sed 's/^\./0./' )
+                    values[1]=$(echo ${values[1]} + ${split[1]} | bc -l | sed 's/^\./0./' )
                     break
                 fi
             done
-            (( counter++ ))
         done <"$FILE"
         for i in {0..1}
         do
